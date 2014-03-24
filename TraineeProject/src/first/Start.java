ab443ddd65f00b3e5c76ce553481df5328cddd9f
MohamedNabil <mohamed.nabil@silicon-nile.com>
2014-03-24 11:12:13 +0200

    added method addNumber

--- TraineeProject/src/first/Start.java
+++ TraineeProject/src/first/Start.java
@@ -4,8 +4,15 @@ public class Start {
 	
 	String name = "";
 	
+	
 	public void showName(String name1) {
 		System.out.println(name1 +", "+name );
+	}
+
+	public int addNumber(int x,int y)
+	{
+			int sum =x + y;
+			return sum;
 	}
 
 }
